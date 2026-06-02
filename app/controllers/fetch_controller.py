fb23fe73707ca03707da19d6ea41c398852ad625
Alizaf <161627817+Alizalazar9@users.noreply.github.com>
2026-06-02 11:13:53 +0300

    fetch controller  fix

--- app/controllers/fetch_controller.py
+++ app/controllers/fetch_controller.py
@@ -8,5 +8,7 @@ from app.services.app_service import Apps
 class FetchController(Resource):  # type: ignore[misc]
     def get(self) -> tuple[dict[str,dict[str, object]],int]:
         apps = Apps()
-        apps.fetch_landing_page()
+        code = apps.fetch_landing_page()
+        if code != 200:
+            return {}, code
         return {id: app.json() for (id, app) in apps._apps.items()}, 200
